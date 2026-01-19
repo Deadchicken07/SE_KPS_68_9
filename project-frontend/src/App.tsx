6c31842517bb83e4fae4b4633c4217eb4caa33f9
Supakorn <deadchicken07@gmail.com>
2026-01-19 22:09:46 +0700

    seccond-commit

---
+++ project-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import TestHome from "./pages/test/Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/test" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
+
