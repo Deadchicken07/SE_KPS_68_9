6a880497158b76217cb6af7894603b216ab23b27
Supakorn <deadchicken07@gmail.com>
2026-02-22 21:29:45 +0700

    feat: update user layout and home page design, enhance navigation with new header structure and add background image

--- project-se-front/app/user/layout.tsx
+++ project-se-front/app/user/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react"
 import NavTop from "@/components/nav/navTop";
+import { Content } from "antd/es/layout/layout";
 
 export default function UserLayout({ 
     children,
@@ -7,9 +8,11 @@ export default function UserLayout({
     children: ReactNode
 }) {
   return (
-    <div>
-      <NavTop/>
-      <main>{children}</main>
-    </div>
+  <div >
+    <NavTop/>
+    <Content style={{ margin: 0, padding: 24 }}>
+        {children}
+    </Content>
+  </div>
   )
 }
--- project-se-front/app/user/page.tsx
+++ project-se-front/app/user/page.tsx
@@ -1,7 +1,60 @@
-export default function home(){
-    return(
-        <div>
-            Hello world
-        </div>
-    );
+export default function Home() {
+  return (
+    <section
+      style={{
+        width: "100%",
+        height: "420px",
+        position: "relative",
+        backgroundImage:
+          "url('https://picsum.photos/1600/800')", 
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        borderRadius: 20,
+        overflow: "hidden",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          inset: 0,
+          background: "rgba(0,0,0,0.35)",
+        }}
+    />
+      <div
+        style={{
+          position: "relative",
+          zIndex: 2,
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          paddingLeft: 80,
+          color: "#fff",
+        }}
+      >
+        <h1 style={{ fontSize: 42, fontWeight: "bold", marginBottom: 16 }}>
+          ไม่รู้ตั้งอะไรดี
+        </h1>
+
+        <p style={{ fontSize: 18, marginBottom: 24 }}>
+          ใส่ไรดีวะ
+        </p>
+
+        <button
+          style={{
+            width: 160,
+            height: 45,
+            backgroundColor: "#0f766e",
+            border: "none",
+            borderRadius: 30,
+            color: "#fff",
+            fontWeight: "bold",
+            cursor: "pointer",
+          }}
+        >
+          ดูรายละเอียด
+        </button>
+      </div>
+    </section>
+  );
 }
