f6f6cb67e3a6d714e59dba2f244804308dbbbe2f
Trai2548 <22trai2546@gmail.com>
2026-03-14 13:50:12 +0700

    edit

--- project-se-front/app/layout.tsx
+++ project-se-front/app/layout.tsx
@@ -1,36 +1,36 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import ClinicLayout from "../components/nav/navTop";
-import "./user/globals.css";
+// import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
+// import ClinicLayout from "../components/nav/navTop";
+// import "./user/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
-export const metadata: Metadata = {
-  title: "JitDee.com",
-  description: "JitDee.com",
-};
+// export const metadata: Metadata = {
+//   title: "JitDee.com",
+//   description: "JitDee.com",
+// };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ClinicLayout />
-        {children}
-      </body>
-    </html>
-  );
-}
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         <ClinicLayout />
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
